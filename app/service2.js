4f2cd5a81fa59f29b93ec65b6e8f65e0d0a3968c
lauri-ra <lrautiala+1@gmail.com>
2023-09-25 17:46:03 +0300

    Updated service 2: parses data from s2 mostly correctly

--- app/service2.js
+++ app/service2.js
@@ -1,23 +1,67 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 app.use(express.json());
 
 const PORT = 8000;
+const LOGPATH = '../logs/service2.log';
 
-// Test route
-app.get('/ping', (request, response) => {
-	response.send('pong');
-});
+// Function for writing the text to a file
+const writeToFile = (text) => {
+	fs.appendFile(LOGPATH, text, (error) => {
+		if (error) {
+			console.log(error);
+		}
+	});
+};
 
-// Route for receiving data from service 1
-app.post('/', (request, response) => {
-	const text = request.body.text;
-	console.log('Received:', text);
-	response.send('POST request received');
-});
+// Function  for initalizing the file
+const initializeLogFile = (filePath) => {
+	// If the log file doesn't exist, create a new empty one
+	if (!fs.existsSync(filePath)) {
+		fs.writeFileSync(filePath, '');
+	} else {
+		// If the file has stuff in it -> empty it
+		if (fs.statSync(filePath).size > 0) {
+			fs.truncateSync(filePath, 0);
+		}
+	}
+};
 
-// Run the server on port 8000
-app.listen(PORT, () => {
-	console.log(`server running on ${PORT}`);
-});
+// Wait for 2 seconds before running the server
+setTimeout(() => {
+	// Initialize the log file
+	initializeLogFile(LOGPATH);
+
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+
+	// Route for receiving data from service 1
+	app.post('/', (request, response) => {
+		const data = request.body.text;
+
+		// If we get STOP signal from s1 -> close the file and exit
+		if (data === 'STOP') {
+			console.log('received STOP');
+
+			// 1s timeout so service 1 has time to exit its process
+			// after 20 rounds, otherwise it will crash
+			setTimeout(() => {
+				console.log('Service stopped.');
+				process.exit();
+			}, 1000);
+		} else {
+			// Parse the new log text together
+			const remoteAddress = 'Service 1 address';
+			const text = data.replace(/\n/g, '');
+			const newLogText = text + ' ' + remoteAddress + '\n';
+
+			// Write the data to the logfile
+			writeToFile(newLogText);
+		}
+
+		response.send('POST request received');
+	});
+}, 2000);
